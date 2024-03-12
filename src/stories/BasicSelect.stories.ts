fc21179e9229a5765f4a293e4f722afdbfedb8f5
Asia.Bielawska <joanna.bielawska8@gmail.com>
2024-03-12 18:33:59 +0100

    Add storybook

---
+++ src/stories/BasicSelect.stories.ts
@@ -0,0 +1,41 @@
+import { fn } from "@storybook/test";
+import { BasicSelect } from "../components/BasicSelect/BasicSelect";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "PetsFinderUI/BasicSelect",
+  component: BasicSelect,
+  parameters: {
+    // Center the component in the Canvas.
+    layout: "centered",
+  },
+  // // Automatically generated Autodocs
+  tags: ["autodocs"],
+  // // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  //   argTypes: {
+  //     value: { control: "number" },
+  //   },
+  // // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {
+    onChange: fn(),
+    label: "Defaultowa",
+  },
+} satisfies Meta<typeof BasicSelect>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Przykladowo: Story = {
+  args: {
+    options: ["1", "2"],
+    value: "1",
+  },
+};
+
+export const Selekt: Story = {
+  args: {
+    options: ["One", "Two", "Three"],
+    value: "One",
+    label: "Number",
+  },
+};
